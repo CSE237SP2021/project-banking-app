2958b2a4e519120700ee11e74e39e538172bdd9d
evanmolyneaux <evanmolyneaux@wustl.edu>
2021-03-26 10:53:06 -0500

    added class with card specifications

---
+++ BankingApp/src/Account.java
@@ -0,0 +1,32 @@
+import java.math.BigDecimal;
+
+public class Account {
+    BigDecimal balance = new BigDecimal(0);
+    long accountNumber = 0;
+
+    public Account() {
+        // randomly generate accountNumber here later
+    }
+
+    /**
+     * increments the account balance by depositAmount
+     * 
+     * @param depositAmount
+     * @return the new account balance
+     */
+    BigDecimal deposit(BigDecimal depositAmount) {
+        this.balance.add(depositAmount);
+        return this.balance;
+    }
+
+    /**
+     * Decrements the account balance by withdrawAmount
+     * 
+     * @param withdrawAmount
+     * @return the new account balance
+     */
+    BigDecimal withdraw(BigDecimal withdrawAmount) {
+        this.balance.subtract(withdrawAmount);
+        return this.balance;
+    }
+}
