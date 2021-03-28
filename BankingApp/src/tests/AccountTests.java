8b4349038634915f78231d1284cb80ca3ff81fbc
jasonren5 <jasonren72@gmail.com>
2021-03-27 22:16:09 -0500

    added test for deposit

--- BankingApp/src/tests/AccountTests.java
+++ BankingApp/src/tests/AccountTests.java
@@ -1,14 +1,27 @@
 package tests;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.math.BigDecimal;
+
+import main.Account;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class AccountTests {
-
+	
+	private Account account;
+	
+	@BeforeEach
+	public void setupTestingObjects() {
+		account = new Account();
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testDeposit() {
+		BigDecimal depositAmount = new BigDecimal("12.34");
+		assertTrue(depositAmount.compareTo(account.getBalance()) == 0);
 	}
 
 }
