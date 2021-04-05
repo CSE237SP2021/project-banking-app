63473c5208c028e867f5c4bdae6586a8bedc6554
jasonren5 <jasonren72@gmail.com>
2021-04-04 19:52:57 -0500

    created test for register

---
+++ BankingApp/src/tests/AuthTests.java
@@ -0,0 +1,35 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import main.Auth;
+
+public class AuthTests {
+	private Auth auth;
+	private String basePath;
+	
+	@BeforeEach
+	public void setupTestingObjects() {
+		auth = new Auth();
+		basePath = "./data/";
+	}
+	
+	@Test
+	/*
+	 * creates a user and tests if the directory exists. then tears down the test
+	 */
+	public void testRegister() {
+		String id = "000000";
+		String password = "testpw";
+		String firstName = "John";
+		String lastName = "Doe";
+		
+		auth.register(id, password, firstName, lastName);
+		
+		File userDirectory = new File(basePath + id);
+		assertTrue(userDirectory.isDirectory());
+	}
+}
