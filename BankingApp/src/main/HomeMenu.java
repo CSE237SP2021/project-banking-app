66cff4f910333aa74c330633cf075a125f115978
evanmolyneaux <evanmolyneaux@wustl.edu>
2021-04-13 15:55:58 -0500

    added select account functionality

--- BankingApp/src/main/HomeMenu.java
+++ BankingApp/src/main/HomeMenu.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class HomeMenu {
@@ -8,6 +9,7 @@ public class HomeMenu {
 	private final String NEW_ACCOUNT = "newaccount";
 	private final String CANCEL = "cancel";
 	private final String EXIT = "exit";
+	private final String SELECT = "selectaccount";
 	
 	private Person person;
 	
@@ -33,6 +35,11 @@ public class HomeMenu {
 			case EXIT:
 				System.out.println("Thank you for banking with us, goodbye");
 				break mainLoop;
+			case SELECT:
+				selectAccountHandler(scanner);
+				break;
+			default:
+				System.out.println("Command not recognized");
 				
 			}
 		}
@@ -51,7 +58,7 @@ public class HomeMenu {
 					: "Please enter a unique name for your new account or \"cancel\" to cancel");
 					
 			input = scanner.next();
-			System.out.println("INPUT" + input);
+			
 			if(input.toLowerCase().equals(CANCEL)) return;
 			
 			success = this.person.addAccount(input);
@@ -59,8 +66,53 @@ public class HomeMenu {
 			
 		} while(success == false);
 		
-		System.out.println("A new account was created with name: " + input);
+		System.out.println("A new account was created with name: " + input);	
+	}
+	/**
+	 * Handles selecting account functionality
+	 * 
+	 * @param scanner
+	 */
+	private void selectAccountHandler(Scanner scanner) {
+		String input;
+		boolean success = false;
+		do {
+			System.out.println("Available accounts are: ");
+			
+			for(Account acc : person.getAccounts()) {
+				System.out.print(acc + " ");
+			}
+			
+			System.out.println("");
+			System.out.println("Enter account name to select that account");
+			
+			input = scanner.next();
+			int index = findAccount(input);
+			if(index == -1) {
+				System.out.println("Error, account \"" + input + "\" not found");
+			}else {
+				Account selectedAccount = person.getAccounts().get(index);
+				AccountsMenu accountsMenu = new AccountsMenu(selectedAccount);
+				accountsMenu.start(scanner);
+				success = true;
+			}
+			
+		} while(success == false);
 		
+	}
+	/**
+	 * 
+	 * @param accountName
+	 * @return index of account associated with accountName in the person accounts list, or -1 if not found
+	 */
+	private int findAccount(String accountName) {
+		List<Account> accounts = person.getAccounts();
+		for(int i = 0; i < accounts.size(); i++) {
+			if(accounts.get(i).getAccountName().equals(accountName)) {
+				return i;
+			}
+		}
+		return -1;
 	}
 	
 	
