139100604b10c4940ee207a1f594e21ccb7f71ce
jasonren5 <jasonren72@gmail.com>
2021-04-13 23:19:28 -0500

    Changed Menu to abstract, added abstract start method

--- BankingApp/src/main/Menu.java
+++ BankingApp/src/main/Menu.java
@@ -3,22 +3,28 @@ package main;
 import java.math.BigDecimal;
 import java.util.Scanner;
 
-public class Menu {
+/**
+ * Parent class to all Menu classes -- all Menu classes should extend Menu
+ * Contains helper methods
+ */
+public abstract class Menu {
 
 	/**
 	 * Gets input until a valid BigDecimal is input by the user
+	 * 
 	 * @param scanner
 	 * @return valid BigDecimal input
 	 */
 	protected BigDecimal inputBigDecimal(Scanner scanner) {
 		String inputValue = new String();
-		
+
 		while (!scanner.hasNextBigDecimal()) {
 			inputValue = scanner.next();
-			System.out.println("Please enter a valid decimal value.");	
+			System.out.println("Please enter a valid decimal value.");
 		}
 		return scanner.nextBigDecimal();
 	}
-	
-	
+
+	public abstract void start(Scanner _scanner);
+
 }
